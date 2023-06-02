249fa1df19677f001f75d4ae0fdfcf7b7278812b
Ariel-ludenyo <124559795+Ariel-ludenyo@users.noreply.github.com>
2023-06-02 17:36:15 +0300

    Add files via upload

---
+++ OTHERS/hello world.py
@@ -0,0 +1,16 @@
+from ast import If
+import random 
+print('Are you an enterprenure? ')
+    #print ("What kind of business do you want to invest in")
+("I love car bussines ")
+# print('Matatu travelling from  western kenya')
+
+(" Buy One 32-seater matatu")
+print('Let the matatu travel from wester kenya to the city')
+("After every week take the car to under go service check at the guarage")
+print("Pay the driver at every month ")
+(" Buy another 45-seater car as a backup travelling the same route")
+
+print("continue buying matatus every year from the profits that you make")
+print('From the matatu business you can invest other Bussinesses like oppenning a big hotell')
+print("Ilove investing because through investing you gain alot of things")
